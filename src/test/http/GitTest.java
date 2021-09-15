3472b04c11a12221df59bc6cbb5d0e238e56c939
myajit <33442182+myajit@users.noreply.github.com>
2021-09-15 20:07:33 +0900

    conflict test

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -2,7 +2,7 @@ package test.http;
 
 public class GitTest {
 	
-	private static final String DESC = "local GIT TEST용 클래스 입니다.";
+	private static final String DESC = "GITHUB local GIT TEST용 클래스 입니다.";
 	private static final String DEV = "dev브랜치 테스트 시작";
 	private static final String DEV1 = "dev브랜치 테스트 다시시작";
 	
